b2c7f68e280e4ff4006d94dcac7f00b534675676
Nick Whitelegg <nickw4426@gmail.com>
2025-09-23 12:36:02 +0100

    clean up package.json

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    // base: '/lilt/', // uncomment if you have a non-default base dir e.g. https://hikar.org/lilt
+    // base: '/lilt/', // uncomment if you have a non-default base dir e.g. https://example.com/lilt
     plugins: [react()],
     port: 3002,
     resolve: { external: ["iron-session"] }
