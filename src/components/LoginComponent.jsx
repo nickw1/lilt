52db2093ffeef14e9f15d7f7a662f488be5b6951
Nick Whitelegg <nickw4426@gmail.com>
2025-10-16 13:27:12 +0100

    Further login UI improvements

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -13,17 +13,17 @@ export default function LoginComponent({usercode}) {
     const { navigate } = useClient();
 
     return usercode === null ? 
-         (newUserStage === 1 && newUserState === null ?  <form action={newUserWithState}>
+         (newUserStage === 1 && newUserState === null ?  <form action={newUserWithState} onSubmit={() => setNewUserStage(2)}>
         <p style={{fontSize: '75%'}}><strong>Privacy notice:</strong> Your answers to questions, and progress (which questions you have answered) will be stored on the lilt server and linked to your user code, which is valid for one week. After one week, this information will be deleted. <strong>No personally identifiable information, for example name or email address, will be stored at all. This is a completely anonymous tool; user codes are randomly generated.</strong></p>
         <input type='submit' value='Click to accept the above.' />
         </form>
         :
         <>
-        <form action={loginWithState} key='formLogin'>
+        <form action={loginWithState} key='formLogin' onSubmit={() => setNewUserStage(0)}>
         <div id='newUserStatus' style={{fontSize: "60%", fontWeight: "bold", backgroundColor : newUserState?.usercode ? '#c0ffc0' : '#ffc0c0' }}>{newUserState?.usercode ? `Your user code is ${newUserState.usercode} and is valid for one week, please login` : (newUserState?.error || "")}</div>
         User code: <input id='usercode' name='usercode' />
         <input type='submit' value='Login'  />
-        <div id='loginError' style={{backgroundColor: '#ffc0c0'}}>{loginState?.error || ""}</div>
+        <div id='loginError' style={{backgroundColor: '#ffc0c0'}}>{newUserStage < 2 && loginState?.error ? loginState.error : ""}</div>
         </form>
         <input type='button' value='Get New User Code' onClick={ () => setNewUserStage(1) } />
         </>)
