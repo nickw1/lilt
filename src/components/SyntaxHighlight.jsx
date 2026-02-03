abd0e54fcf180ed64b613980b844a0ce8162fa70
Nick Whitelegg <nickw4426@gmail.com>
2026-02-03 12:33:58 +0000

    quick fix for <script> inclusion issue in Markdown samples

--- src/components/SyntaxHighlight.jsx
+++ src/components/SyntaxHighlight.jsx
@@ -6,15 +6,22 @@ import { Interweave } from 'interweave';
 
 export default function SyntaxHighlight({children, lang}) {
     const [html, setHtml] = useState("");
-//	console.log(JSON.stringify(children));
+    // <script> tags in Markdown code blocks cause problems with rendering
+    // They need to be entity-encoded (&lt; and &gt;) in the code blocks.
+    // Then we unencode them here, then codeToHtml() encodes them again
+    const unencodedChildren = children.replaceAll(/&lt;script(.*)&gt;(.*)&lt;\/script&gt;/g,`<script$1>$2</script>`);
     useEffect(() => {
  //       const lang = props.className?.includes('lang-') ? props.className.substr(5) : "";
-        const html = codeToHtml(children, {
+        const html = codeToHtml(unencodedChildren, {
             lang,
             theme: 'vitesse-dark'
-        }).then(html => setHtml(html));
+        }).then(html => setHtmlWithUnescape(html));
     }, []);
 
     return <Interweave content={html} />
+
+    function setHtmlWithUnescape(html) {
+        setHtml(html);
+    }
 }
 
